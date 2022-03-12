39dabd62e7e90d250d84b711cc7c138c0fbc35cf
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-03-12 22:30:15 +0600

    Create the Login component and Adding React Hooks
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import Register from './Register';
+import Login from './Login';
+// import Register from './Register';
 
 function App() {
   return (
     <main className="App">
-      <Register />
+      {/* <Register /> */}
+      <Login />
     </main>
   );
 }
---
+++ client/src/Login.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+export default function Login() {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [user, pwd]);
+
+  return <div>Login</div>;
+}
