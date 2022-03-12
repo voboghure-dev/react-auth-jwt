0983748a259d8a248e3c58b588b7fbdfe1518570
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-03-12 22:37:42 +0600

    Finish login JSX
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- client/src/Login.js
+++ client/src/Login.js
@@ -17,5 +17,58 @@ export default function Login() {
     setErrMsg('');
   }, [user, pwd]);
 
-  return <div>Login</div>;
+  return (
+    <>
+      {success ? (
+        <section>
+          <h1>You are logged in!</h1>
+          <br />
+          <p>
+            <a href='#'>Go to Home</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? 'errmsg' : 'offscreen'}
+            aria-live='assertive'
+          >
+            {errMsg}
+          </p>
+          <h1>Sign In</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor='username'>Username:</label>
+            <input
+              type='text'
+              id='username'
+              ref={userRef}
+              autoComplete='off'
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+              required
+            />
+
+            <label htmlFor='password'>Password:</label>
+            <input
+              type='password'
+              id='password'
+              onChange={(e) => setPwd(e.target.value)}
+              value={pwd}
+              required
+            />
+            <button>Sign In</button>
+          </form>
+          <p>
+            Need an Account?
+            <br />
+            <span className='line'>
+              {/*put router link here*/}
+              <a href='#'>Sign Up</a>
+            </span>
+          </p>
+        </section>
+      )}
+    </>
+  );
 }
