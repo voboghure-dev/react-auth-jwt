6c7fe2490a4adad929bd6da0bb7b716ee3e1963d
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-03-15 17:14:25 +0600

    Update .editorconfig file and start AuthController using sanctum
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- api/routes/api.php
+++ api/routes/api.php
@@ -13,15 +13,17 @@ use App\Http\Controllers\UserController;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware( 'auth:sanctum' )->get( '/user', function ( Request $request ) {
+  return $request->user();
+} );
 
-Route::post('user/create', [UserController::class, 'create']);
-Route::get('user/list', [UserController::class, 'list']);
-Route::delete('user/delete/{id}', [UserController::class, 'delete']);
-Route::get('user/{id}', [UserController::class, 'get']);
-Route::post('user/update', [UserController::class, 'update']);
-Route::post('user/login', [UserController::class, 'get_by_email_password']);
+Route::post( '/register', [AuthController::class, 'register'] );
+
+Route::post( 'user/create', [UserController::class, 'create'] );
+Route::get( 'user/list', [UserController::class, 'list'] );
+Route::delete( 'user/delete/{id}', [UserController::class, 'delete'] );
+Route::get( 'user/{id}', [UserController::class, 'get'] );
+Route::post( 'user/update', [UserController::class, 'update'] );
+Route::post( 'user/login', [UserController::class, 'get_by_email_password'] );
