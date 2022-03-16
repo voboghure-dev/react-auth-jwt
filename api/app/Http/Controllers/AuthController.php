45ed367d0c6862a353ef78b8ae5a2f90266e7628
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-03-16 16:07:35 +0600

    Update migration, model and implement registration api call
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -2,6 +2,27 @@
 
 namespace App\Http\Controllers;
 
-class AuthController extends Controller {
+use App\Models\User;
+use Illuminate\Http\Request;
 
+class AuthController extends Controller {
+  public function register( Request $request ) {
+    $request->validate( [
+      'name'     => ['required', 'string'],
+      'email'    => ['required', 'string', 'unique:users,email'],
+      'password' => ['required', 'string'],
+    ] );
+
+    $user = User::create( [
+      'name'     => $request->name,
+      'email'    => $request->email,
+      'password' => bcrypt( $request->password ),
+    ] );
+
+    $token = $user->createToken( 'ourapptoken' )->plainTextToken;
+
+    $response = ['user' => $user, 'token' => $token];
+
+    return response( $response, 201 );
+  }
 }
